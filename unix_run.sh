f3c156385b6922bfaf1aa08ca77e914825b0d203
Quentin Quadrat <lecrapouille@gmail.com>
2024-02-04 14:57:25 +0100

    Update compilation script for Unix

--- unix_run.sh
+++ unix_run.sh
@@ -1,4 +1,16 @@
 cmake -G "Unix Makefiles" -B "./build" .
+
 cd build
-make
+if [ "$(uname -s)" == "Linux" ]; then
+  make -j`nproc --all`
+elif [ "$(uname -s)" == "Darwin" ]; then
+  make -j`sysctl -n hw.logicalcpu`
+else
+  make -j8
+fi
+RES=`echo $?`
+
 cd ..
+if [ "$RES" == "0" ]; then
+  ./build/Janus
+fi
